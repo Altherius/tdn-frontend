925a2a39576ee579872f69418ff351a96047cf0a
Maxime Eychenne <maxime.eychenne@gmail.com>
2025-01-03 15:34:03 +0100

    add country code as flag placeholders

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,6 +3,7 @@ import { Game } from '../types/game';
 export default function GameCard({game}: {game: Game}) {
     return <div>
         <h3>{game.name}</h3>
+        {game.hostingTeam.countryCode} - {game.receivingTeam.countryCode}
         <p>{game.gameScoreTotal} (Aller {game.gameScoreFirst} - Retour {game.gameScoreSecond})</p>
     </div>;
 }
