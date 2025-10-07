0e9bd80e3a40ec91a7ff7f3a800d03c2238031e6
Maxime Eychenne <maxime.eychenne@gmail.com>
2025-10-07 17:13:49 +0200

    update flag emoji with special flags

--- src/functions/getFlagEmoji.ts
+++ src/functions/getFlagEmoji.ts
@@ -1,4 +1,16 @@
 export default function getFlagEmoji(countryCode: string) {
+
+    const specialFlags: Record<string, string> = {
+        'GB-ENG': '🏴󠁧󠁢󠁥󠁮󠁧󠁿',
+        'GB-SCT': '🏴󠁧󠁢󠁳󠁣󠁴󠁿',
+        'GB-WLS': '🏴󠁧󠁢󠁷󠁬󠁳󠁿',
+    };
+
+    // Return special flags directly if they exist
+    if (specialFlags[countryCode.toUpperCase()]) {
+        return specialFlags[countryCode.toUpperCase()];
+    }
+
     return [...countryCode.toUpperCase()].map(char =>
         String.fromCodePoint(127397 + char.charCodeAt(0))
     ).reduce((a, b) => `${a}${b}`);
